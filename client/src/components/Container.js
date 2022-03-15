b124f81466930d6d36a022fe125c3f9b9797244e
ufukcankurt <ufuk.cnk@gmail.com>
2022-03-15 07:37:03 +0300

    Create backend connection

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import ChatForm from './ChatForm'
-import ChatList from './ChatList'
+import React, { useEffect } from "react";
+import ChatForm from "./ChatForm";
+import ChatList from "./ChatList";
+
+import { init } from "../socketApi";
 
 function Container() {
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
-    <div className='App'>
-        <ChatList/>
-        <ChatForm/>
+    <div className="App">
+      <ChatList />
+      <ChatForm />
     </div>
-  )
+  );
 }
 
-export default Container
+export default Container;
