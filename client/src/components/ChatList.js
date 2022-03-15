781ecf4ab461174cdb864a94c7929a8dfd38bf3b
ufukcankurt <ufuk.cnk@gmail.com>
2022-03-15 08:37:52 +0300

    Create | Feed scroll

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -2,17 +2,18 @@ import React from "react";
 import { useChat } from "../context/ChatContext";
 import styles from "./styles.module.css";
 import ChatItem from "./ChatItem";
+import ScrollableFeed from 'react-scrollable-feed'
 
 function ChatList() {
   const { messages } = useChat();
 
   return (
     <div className={styles.chatlist}>
-      <div>
+      <ScrollableFeed forceScroll={true}>
         {messages.map((item, key) => (
           <ChatItem key={key} item={item} />
         ))}
-      </div>
+      </ScrollableFeed>
     </div>
   );
 }
